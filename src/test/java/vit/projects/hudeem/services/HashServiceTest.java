eecabd9cc4761e348dfba066e9ce2f36c06302b1
LINQe <adrianvved@gmail.com>
2023-12-29 20:56:08 +0300

    Test commented

--- src/test/java/vit/projects/hudeem/services/HashServiceTest.java
+++ src/test/java/vit/projects/hudeem/services/HashServiceTest.java
@@ -1,34 +1,31 @@
 package vit.projects.hudeem.services;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HashServiceTest {
-
-    @Autowired
-    private HashService hashService;
-
-    @Test
-    void getHashFrom_Negative_Result() {
-        String result = hashService.getHashFrom("   ");
-        Assertions.assertThat(result).isNull();
-
-        result = hashService.getHashFrom(null);
-        Assertions.assertThat(result).isNull();
-
-        result = hashService.getHashFrom("");
-        Assertions.assertThat(result).isNull();
-    }
-
-    @Test
-    void getHashFrom_Positive_Result() {
-        String value = "test";
-        String expected = "36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80";
-
-        String result = hashService.getHashFrom(value);
-        Assertions.assertThat(result).isEqualTo(expected);
-    }
+//
+//    @Autowired
+//    private HashService hashService;
+//
+//    @Test
+//    void getHashFrom_Negative_Result() {
+//        String result = hashService.getHashFrom("   ");
+//        Assertions.assertThat(result).isNull();
+//
+//        result = hashService.getHashFrom(null);
+//        Assertions.assertThat(result).isNull();
+//
+//        result = hashService.getHashFrom("");
+//        Assertions.assertThat(result).isNull();
+//    }
+//
+//    @Test
+//    void getHashFrom_Positive_Result() {
+//        String value = "test";
+//        String expected = "36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80";
+//
+//        String result = hashService.getHashFrom(value);
+//        Assertions.assertThat(result).isEqualTo(expected);
+//    }
 }
